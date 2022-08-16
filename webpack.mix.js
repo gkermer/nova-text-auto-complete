73afacd9a68095b70cec184212e269ac95005da3
Dimitrios Chartavellas <dimitrios@chartavellas.com>
2022-08-16 23:13:08 +0300

    Nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,10 @@
 let mix = require('laravel-mix')
 
-mix.setPublicPath('dist')
-   .js('resources/js/field.js', 'js')
-   .sass('resources/sass/field.scss', 'css')
+require('./nova.mix')
+
+mix
+  .setPublicPath('dist')
+  .js('resources/js/field.js', 'js')
+  .vue({ version: 3 })
+  .sass('resources/sass/field.scss', 'css')
+  .nova('gkermer/nova-text-auto-complete')
